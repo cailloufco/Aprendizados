05f7246bb6cbdb273ab04f75561e210370e43dba
cailloufco <caioextreme000@gmail.com>
2025-12-22 22:58:43 -0300

    mudanças visuais

--- JavaScript/Aula5/2/exercisio.js
+++ JavaScript/Aula5/2/exercisio.js
@@ -1,25 +1,24 @@
-data = new Date()
-anoatual = Number(data.getFullYear())
-console.log(data, anoatual)
-function verificar(){
-    let sidade = Number(document.getElementById("Idade").value)
-    let resultado = document.getElementById("resul")
-    let sexo = document.querySelector('input[name="genero"]:checked')
-    //calculo da idade.....
-    calculoidade = (anoatual - sidade)
+data = new Date();
+anoatual = Number(data.getFullYear());
+console.log(data, anoatual);
+function verificar() {
+  let sidade = Number(document.getElementById("Idade").value);
+  let resultado = document.getElementById("resul");
+  let sexo = document.querySelector('input[name="genero"]:checked');
+  //calculo da idade.....
+  calculoidade = anoatual - sidade;
 
-if ( sidade >= anoatual ) {
-    resultado.innerHTML=(`Idade invalida!`)
-    resultado.style.color="red"
-}else if (sidade < 1925){
-    resultado.innerHTML=(`Idade invalida!`)
-    resultado.style.color="red"
-}else if(isNaN(sidade)){
-    resultado.innerHTML=(`Idade invalida!`)
-    resultado.style.color="red"
-}
-else{
-    resultado.innerHTML=(`${calculoidade} anos do sexo ${sexo.value}`)
-    resultado.style.color="black"
-}
+  if (sidade >= anoatual) {
+    resultado.innerHTML = `Idade invalida!`;
+    resultado.style.color = "red";
+  } else if (sidade < 1925) {
+    resultado.innerHTML = `Idade invalida!`;
+    resultado.style.color = "red";
+  } else if (isNaN(sidade)) {
+    resultado.innerHTML = `Idade invalida!`;
+    resultado.style.color = "red";
+  } else {
+    resultado.innerHTML = `${calculoidade} anos do sexo ${sexo.value}`;
+    resultado.style.color = "black";
+  }
 }
