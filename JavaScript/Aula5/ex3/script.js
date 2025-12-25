c75ecc38aeeee5b364972d546356e1c10774b4eb
cailloufco <caioextreme000@gmail.com>
2025-12-25 18:01:35 -0300

    organizando as condiçoes e adicionando muçando de background de acordo com a hora

--- JavaScript/Aula5/ex3/script.js
+++ JavaScript/Aula5/ex3/script.js
@@ -1,3 +1,4 @@
+function atualizarHoraSite(){
 //horario de quem ve o site
 data = new Date();
 horasatuais = data.getHours();
@@ -32,15 +33,26 @@ if (segundosatuaois < 10) {
 }
 //==============================
 
-//fazer imagem do site alterar de acordo com a hora
+//fazer imagem do site alterar de acordo com a hora /alterar background de acordo com a hora
 imagemDoSite = document.getElementById("pngDiaTardeNoite");
-
-if (horasatuais <= 6) {
+corDoBackGround = document.getElementById("background")
+if (horasatuais < 6) {
   imagemDoSite.src = "assets/madrugada.jpg";
-} else if (horasatuais <= 12) {
+  document.body.style.backgroundColor = "rgba(0, 0, 0, 1)"
+
+} else if (horasatuais < 12) {
   imagemDoSite.src = "assets/dia.jpg";
-} else if (horasatuais <= 18) {
+  document.body.style.backgroundColor ="rgba(185, 173, 0, 0.8)"
+
+} else if (horasatuais < 18) {
   imagemDoSite.src = "assets/tarde.jpg";
-} else if(horasatuais <= 24){
+  document.body.style.backgroundColor = "rgba(153, 103, 10, 0.88)"
+
+} else if(horasatuais < 24){
   imagemDoSite.src = "assets/noite.jpg";
+  document.body.style.backgroundColor = "rgba(31, 30, 28, 0.93)"
+
 }
+
+}
+setInterval (atualizarHoraSite, 600)
